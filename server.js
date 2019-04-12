39df1e9d3ef213aa68668cd6a2aaf6c951fcfdac
Chanon Treemeth <swu59565@outlook.com>
2019-04-12 21:22:28 +0700

    new uploadfile

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./firebase/serviceAccountKey_firebase.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://voyage-cd39b.firebaseio.com"
+});
+
+
+app.get('/',function(req,res) {
+    res.sendFile(__dirname+'/Design/load_img.html');
+  });
+
+
+
+
+
+
+
+app.listen(8081);
+
