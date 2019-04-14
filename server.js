393c93fd6fb06c66f9f721e4f34f4ba540ebfe10
Jirawat promsee <swu59898@outlook.com>
2019-04-14 13:47:42 +0700

    edit template signup/create button

--- server.js
+++ server.js
@@ -32,26 +32,35 @@ admin.initializeApp({
 app.get('/test_load_img',function(req,res) {
     res.sendFile(__dirname+'/Design/load_img.html');
   });
+app.get('/test',function(req,res){
+  res.sendFile(__dirname+'/Design/index.html');
+});
+
 
 app.get('/',function(req,res){
   //กำหนด ส่วน head ที่ดึงข้อมูลมา
   var head = '';
+  head += '<!DOCTYPE html>'
   head += '<html>';
   head += '<head>';
   head += ' <meta charset="UTF-8">';
   head += ' </head>'
   head += '  <meta name="viewport" content="width=device-width, initial-scale=1">'
   head += '  <link rel="stylesheet" href="./css/home.css">';
-  head += '<script src="./js/popupsign.js"></script>'
   head += '  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">'
   head += '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>'
   head += '<script>$(function(){$("#includedContent").load("./html/home.html");});</script>'
+
   head += ' </header>';
   // ส่วนของ body
   var body = '';
   body += '<body>';
 //ส่วนที่สามารถแก้ไขได้
   body += '<div id="includedContent"></div>'
+
+//ส่วนท้ายของ body
+  body += '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>'
+  body += '<script rel="stylesheet" src="./js/popupsign.js"></script>'
   body += '</body>';
   body += '</html>';
   var html ='';
