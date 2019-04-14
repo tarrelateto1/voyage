d0d1ee8ab04660a90d9883d24e95f43b3ee333e1
Chanon Treemeth <swu59565@outlook.com>
2019-04-15 04:30:30 +0700

    login and sign up success and begin blogger

---
+++ class/blogger.js
@@ -0,0 +1,23 @@
+class Blogger{
+
+
+    constructor(){
+    var User = null
+    var Title = null
+    var Content = null
+    }
+    setuser(user){
+      this.User = user
+    }
+    settitle(title){
+      this.Title = title
+  
+    }
+    setcontent(content){
+      this.Content = content
+  
+    }
+  
+  }
+  module.exports = Blogger;
+  
--- server.js
+++ server.js
@@ -9,11 +9,9 @@ var body = require('body-parser');
 var user = require('./class/User.js');
 // test
 var u = new user();
-u.setuser("tar");
-u.setpass("123");
-u.setstate("user");
 
-console.log(u.user);
+
+
 
 // set api
 app.use(express.static(path.join(__dirname, 'public')));
@@ -27,6 +25,33 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://voyage-cd39b.firebaseio.com"
 });
+// test blogger
+var db = admin.database();
+var ref = db.ref("blogger");
+var t2;
+ref.once("value", function(snapshot) {
+  t2 = snapshot.val();
+  console.log("begin");
+  // console.log(snapshot)
+  console.log("."+snapshot.val())
+  console.log("download success");
+  console.log("show data");
+console.log(t2["a1"]["user"]);
+console.log(Object.keys(t2["a1"]["comment"]))
+var test = Object.keys(t2)
+console.log(Object.keys(t2)[0]);
+console.log(test[1]);
+});
+
+
+//download list user
+var db = admin.database();
+var t1;
+var ref = db.ref("User");
+ref.once("value", function(snapshot) {
+  t1 = snapshot.val();
+});
+
 
 // เหมือนจะไม่ได้ใช้แล้ว
 app.get('/test_load_img',function(req,res) {
@@ -35,8 +60,78 @@ app.get('/test_load_img',function(req,res) {
 app.get('/test',function(req,res){
   res.sendFile(__dirname+'/Design/index.html');
 });
+//register 
+app.post('/register',function(req,res){
+  var ref = db.ref("User");
+  var repeat_id = false;
+  var keys = [];
+  for (var k in t1 )keys.push(k);
+  console.log(keys);
+  for (var i = 0 ; i<keys.length;i++){
+    if(keys[i]==req.body.user){
+      repeat_id = true
+    }
+  }
+  if(!repeat_id){
+    var usersRef = ref.child(String(req.body.user));
+   usersRef.update({
+   "pass": String(req.body.pass),
+   "status" : "User"
+ });
+console.log("success");
+ref.once("value", function(snapshot) {
+  t1 = snapshot.val();
+   console.log(t1);
+});
+res.redirect("/");
+  }else{
+    console.log("can't add")
+    res.send('<html><head></head><body><script>alert("รหัสซ้ำ"); window.location.replace("/");</script></body></html>')
+  }
+  
+});
+//login
+app.post('/login',function(req,res){
+  var t_user = String(req.body.user);
+    var p_user = String(req.body.pass);
+    var check_pass = false;
+    var keys = [];
+    for (var k in t1 )keys.push(k);
+    // console.log(keys);
+    for (var i = 0 ; i<keys.length;i++){
+      if(keys[i]==t_user){
+       check_pass = true
+      }
+    }
+    // console.log("pass");
+    
+    if(check_pass){
+    if(String(t1[t_user]["pass"]) == String(p_user) && String(t1[t_user]["status"]) == String("User")){
+      u.setuser(t_user);
+      u.setpass(p_user);
+     u.setstate("User");
+     res.send('<html><head></head><body><script>alert("login success"); window.location.replace("/");</script></body></html>')
+    //  res.redirect("/");
+   }else{
+    res.send('<html><head></head><body><script>alert("รหัสไม่ถูกต้อง"); window.location.replace("/");</script></body></html>')
+
+   }
+
+  }else{
+     res.send('<html><head></head><body><script>alert("ไม่มี user ในระบบ"); window.location.replace("/");</script></body></html>')
+
+    // res.redirect("/")
+  }
+});
+app.get('/logout',function(req,res){
+u.setuser(null);
+u.setpass(null);
+u.setstate("User");
+res.send('<html><head></head><body><script>alert("logout success"); window.location.replace("/");</script></body></html>')
+});
 
 
+//ส่วนที่แสดงผล
 app.get('/',function(req,res){
   //กำหนด ส่วน head ที่ดึงข้อมูลมา
   var head = '';
@@ -56,11 +151,17 @@ app.get('/',function(req,res){
   var body = '';
   body += '<body>';
 //ส่วนที่สามารถแก้ไขได้
-  body += '<div id="includedContent"></div>'
+if(u.user ==null){
+  body += '<div id="includedContent"></div>';
+}else {
+  body += '<a href="logout"> <button type="button">Click Me!</button> </a>  ';
+}
+  // body += '<div id="includedContent"></div>';
+  // body += '<form><input type="button" value="Put Your Text Here" onclick="window.location.href="http://www.hyperlinkcode.com/button-links.php" /></form>';
 
 //ส่วนท้ายของ body
-  body += '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>'
-  body += '<script rel="stylesheet" src="./js/popupsign.js"></script>'
+  body += '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>';
+  body += '<script rel="stylesheet" src="./js/popupsign.js"></script>';
   body += '</body>';
   body += '</html>';
   var html ='';
